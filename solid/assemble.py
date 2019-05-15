15e700cdb39ad30692f7d1021d58bf5974672505
Ryan.cjw@gmail.com <ryan.cjw@gmail.com>
2019-05-15 08:42:47 -0700

    Added the assemble module

---
+++ solid/assemble.py
@@ -0,0 +1,17 @@
+class assembly():
+    trans = [0,0,0]
+    rot = [0,0,0]
+
+    def __init__(self):
+        self.trans = [0,0,0]
+        self.rot = [0,0,0]
+    
+    # Mate obj1 feature to obj2
+    def concentricMate(self, obj1, obj2):
+        for i in range(0,3):
+            self.trans[i] += obj1.loc[i] - obj2.loc[i]
+
+    def assembly(self):
+        self.part.part = translate(self.trans)(self.part.part)
+        self.part.part = rotate(self.rot)(self.part.part)
+        return self.part.part
